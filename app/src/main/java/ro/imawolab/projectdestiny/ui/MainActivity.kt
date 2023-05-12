29dd0efca9ecc3c7ae3709da2ad6ad726a9019f6
Ovidiu Petru Muntean <ovidiu.muntean@renfieldsoftware.com>
2023-05-12 12:03:11 +0300

    Initial commit

--- app/src/main/java/ro/imawolab/projectdestiny/ui/MainActivity.kt
+++ app/src/main/java/ro/imawolab/projectdestiny/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ro.imawolab.projectdestiny
+package ro.imawolab.projectdestiny.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ro.imawolab.projectdestiny.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
